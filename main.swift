8a69dd272a92c60ca4659904e80974fd2f0c0702
Glitch Wraith <39679141+eevee444@users.noreply.github.com>
2023-05-18 14:52:34 -0700

    Update main.swift
    Added proper syntax

--- main.swift
+++ main.swift
@@ -3,16 +3,16 @@ var recyclableItems = ["metal", "glass", "paper"]
 var compostableItems = ["food", "soiled paper"]
 var landfillItems = ["plastic"]
 // asks user for material of their item
-var material = String(readLine("What is your trash item made of?")!)!
- if material == "metal" or material == "glass" or material == "paper" {
+print("What is your trash item made of?")
+var material = (readLine(strippingNewline: true))
+ if material == "metal" || material == "glass" || material == "paper" {
     print("Your item can be recycled.")
- } elif material == "food" or material == "soiled paper" {
+ } else if material == "food" || material == "soiled paper" {
     print("Your item can be composted.")
- } elif material == "plastic" {
+ } else if material == "plastic" {
     print("Your item goes in the landfill.")
  } else {
     print("Unknown. Put your item in the landfill just to be safe.")
     }
 /* Potential additions:
 - camera support so users can take photos of their item and have the app assess the material in the image to classify it*/
-
